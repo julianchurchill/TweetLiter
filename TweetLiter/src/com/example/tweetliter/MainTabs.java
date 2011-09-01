80feb36ebc47d0494ad85a49f49b7554433f8bce
julianchurchill <chewielouie03@gmail.com>
2011-09-01 23:01:39 +0100

    Added a tab page adapter, need to figure out how to get it to switch between the ListView of the HomeTimeLineActivity and the UserTimeLineActivity - maybe replace the automatic tab screen entirely and control the tabs manually

--- TweetLiter/src/com/example/tweetliter/MainTabs.java
+++ TweetLiter/src/com/example/tweetliter/MainTabs.java
@@ -1,9 +1,16 @@
 package com.example.tweetliter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.TabActivity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TabHost;
@@ -11,6 +18,11 @@ import android.widget.TextView;
 
 public class MainTabs extends TabActivity
 {
+	private ViewPager tabPager = null;
+	private TabPagerAdapter tabPagerAdapter = null;
+	private List<String> tabNames = new ArrayList<String>();
+	private Context context = null;
+
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,21 +36,85 @@ public class MainTabs extends TabActivity
         addTab( UserTimeLineActivity.class, "userTimeLine", "User Time line" );
 
         getTabHost().setCurrentTab(0);
+
+        tabPagerAdapter = new TabPagerAdapter();
+        tabPager = (ViewPager)findViewById( R.id.tabpager );
+        tabPager.setAdapter( tabPagerAdapter );
+        context = this;
     }
 
     private void addTab( Class<?> classType, String specName, String displayName )
     {
 	    TabHost.TabSpec spec = getTabHost().newTabSpec( specName );
-	    spec.setIndicator( createTabView( this, displayName ) );
+	    spec.setIndicator( createTab( displayName ) );
 	    spec.setContent( new Intent().setClass( this, classType ) );
 	    getTabHost().addTab(spec);
+	    tabNames.add( displayName );
     }
     
-    private static View createTabView(final Context context, final String text)
+    private View createTab( final String text )
     {
-	    View view = LayoutInflater.from( context ).inflate( R.layout.tabs_bg, null );
+	    View view = LayoutInflater.from( this ).inflate( R.layout.tabs_bg, null );
 	    TextView tv = (TextView)view.findViewById( R.id.tabsText );
 	    tv.setText( text );
 	    return view;
+	}
+    
+    private class TabPagerAdapter extends PagerAdapter
+    {
+    	private int NUM_VIEWS = 2;
+
+    	@Override
+    	public int getCount()
+    	{
+    		return NUM_VIEWS;
+    	}
+
+    	@Override
+        public Object instantiateItem(View collection, int position)
+    	{
+            TextView tv = new TextView(context);
+            tv.setText("Bonjour PAUG " + position);
+            tv.setTextColor(Color.WHITE);
+            tv.setTextSize(30);
+
+//    		View tv = createTab( tabNames.get( position ) );
+            ((ViewPager) collection).addView(tv,0);
+            
+            return tv;
+        }
+
+		@Override
+		public void destroyItem(View collection, int position, Object view)
+		{
+			((ViewPager) collection).removeView((TextView)view);
+		}
+
+		@Override
+		public void finishUpdate(View arg0)
+		{
+		}
+
+		@Override
+		public boolean isViewFromObject(View view, Object object)
+		{
+			return view == ((TextView)object);
+		}
+
+		@Override
+		public void restoreState(Parcelable arg0, ClassLoader arg1)
+		{
+		}
+
+		@Override
+		public Parcelable saveState()
+		{
+			return null;
+		}
+
+		@Override
+		public void startUpdate(View arg0)
+		{
+		}
 	}
 }
